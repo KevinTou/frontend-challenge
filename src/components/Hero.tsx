28b21d9499fdb0fdceee9faeff8f1955db6c067c
Kevin Tou <toukevin@gmail.com>
2021-01-07 15:07:11 -0800

    WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function Hero({ src }) {
-    return <Image src={src} width="792" height="373" />;
+export default function Hero({ hero }) {
+    const { header, content, cta, src } = hero;
+
+    return (
+        <div className="grid grid-rows-2">
+            <div className="w-100">
+                <h2>{header}</h2>
+                <p>{content}</p>
+                <p>{cta}</p>
+            </div>
+
+            <Image src={src} width="100%" height="100%" className="bg-blue-700" />
+        </div>
+    )
 }
