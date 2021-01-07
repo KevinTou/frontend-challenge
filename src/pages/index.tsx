c7efdbd8ef2dce01a17a75ea3da0c3f0716126c9
Kevin Tou <toukevin@gmail.com>
2021-01-07 14:43:37 -0800

    Retrieved data from cms endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Hero from "../components/Hero";
+import Axios from 'axios';
 
-export default function Home() {
+export default function Home({ hero }) {
     return (
         <div className="container flex items-center mx-auto justify-center">
-            <Hero src="/images/hero-example.png" />
+            <Hero hero={hero} />
         </div>
     )
+}
+
+export async function getStaticProps() {
+    let { data } = await Axios.get('http://localhost:3000/api/cms');
+
+    return {
+        props: {
+            hero: data
+        }
+    }
 }
