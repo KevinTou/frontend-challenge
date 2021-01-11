f30133597391de1f0ea7d8f803a072344d777085
Kevin Tou <toukevin@gmail.com>
2021-01-10 16:20:00 -0800

    Updated styles for hero content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,14 +12,16 @@ interface HeroType {
 
 const Hero: React.FC<HeroType> = ({ hero: { header, content, cta, src } }) => {
     return (
-        <div className="">
-            <div className="">
-                <h1 className="">{header}</h1>
-                <p className="">{content}</p>
+        <div className="flex flex-col md:flex-row md:flex-wrap">
+            <div className="flex flex-wrap flex-col justify-center items-center mt-5 mb-5 text-center">
+                <h1 className="text-3xl font-bold uppercase w-3/4 mb-5">{header}</h1>
+                <p className="w-3/4">{content}</p>
             </div>
-            <div className="">
-                <Image src={src} width="401" height="537" className="" />
-                <button className="bg-yellow-400 rounded p-3 font-bold uppercase">{cta}</button>
+            <div className="flex flex-wrap flex-col-reverse md:flex-col justify-center items-center">
+                <div className="md:w-full mb-5">
+                    <Image src={src} width="401" height="537" />
+                </div>
+                <button className="bg-yellow-400 rounded p-3 font-bold uppercase mb-5 md:mb-7">{cta}</button>
             </div>
         </div>
     )
