0ee51b81726680e12f1e6e220db05c530c858201
Kevin Tou <toukevin@gmail.com>
2021-01-11 16:45:36 -0800

    Fixed announcement bar going under image

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -4,7 +4,7 @@ import StarIcon from './StarIcon';
 
 const AnnouncementBar: React.FC = () => {
   return (
-    <div className="fixed top-0 bg-blue-300 w-full text-center flex justify-center items-center">
+    <div className="fixed top-0 bg-blue-300 w-full text-center flex justify-center items-center z-10">
       <StarIcon size="w-5" />
       <p className="uppercase text-lg font-semibold">Free shipping for first time purchases*</p>
       <StarIcon size="w-5" />
